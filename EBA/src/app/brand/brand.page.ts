6f4389afef92de4858c2e2b0325e6d1e92e4628c
taninpong <taninpong.ch.56@ubu.ac.th>
2020-11-03 15:07:56 +0700

    Implement Brand

--- EBA/src/app/brand/brand.page.ts
+++ EBA/src/app/brand/brand.page.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BrandInfo } from 'src/models/BrandModel';
 
 @Component({
   selector: 'app-brand',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./brand.page.scss'],
 })
 export class BrandPage implements OnInit {
+  brandinfo: BrandInfo = { name: "", bannerUrl: "", playerId: "" };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  getAPIAllData() {
+    var data = this.http.get("http://localhost:32778/Brand");
+    data.subscribe(data => console.log(data)
+    );
+  }
+
+  postapi(name: string, ban: string, player: string) {
+    console.log(this.brandinfo);
+
+    console.log(name, ban, player);
+    this.brandinfo.name = name;
+    this.brandinfo.bannerUrl = ban;
+    this.brandinfo.playerId = player;
+    console.log(this.brandinfo);
+    this.http.post<BrandInfo>('http://localhost:32778/Brand', this.brandinfo).subscribe(data => {
+      console.log(data);
+    })
+  }
+  getAPIById(id: string) {
+    var data = this.http.get("http://localhost:32778/Brand/" + id);
+    data.subscribe(data => console.log(data)
+    );
+  }
+
+  DeleteData(id: string) {
+    var data = this.http.delete("http://localhost:32778/Brand/" + id);
+    data.subscribe(data => console.log("Delete Success")
+    );
+  }
 }
---
+++ EBA/src/models/BrandModel.ts
@@ -0,0 +1,7 @@
+import { BrandPage } from "src/app/brand/brand.page";
+
+export class BrandInfo {
+    name: string;
+    bannerUrl: string;
+    playerId: string;
+}
