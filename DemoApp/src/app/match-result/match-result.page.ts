b8f5553c4615d2d886c667f78e2facb418cb4cec
Chokcha Homsombat <48384299+chokchai9900@users.noreply.github.com>
2020-11-10 12:06:09 +0700

    add Match Result (Captan mode and player mode )

--- DemoApp/src/app/match-result/match-result.page.ts
+++ DemoApp/src/app/match-result/match-result.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-match-result',
-  templateUrl: './match-result.page.html',
-  styleUrls: ['./match-result.page.scss'],
+  selector: "app-match-result",
+  templateUrl: "./match-result.page.html",
+  styleUrls: ["./match-result.page.scss"],
 })
 export class MatchResultPage implements OnInit {
+  public data: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.route.queryParams.subscribe((params) => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.data = this.router.getCurrentNavigation().extras.state.isCaptan;
+      }
+    });
   }
 
+  ngOnInit() {
+    console.log(this.data);
+  }
+  onClick() {}
 }
--- DemoApp/src/app/matching/matching.page.ts
+++ DemoApp/src/app/matching/matching.page.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router, NavigationExtras } from "@angular/router";
 
 @Component({
-  selector: 'app-matching',
-  templateUrl: './matching.page.html',
-  styleUrls: ['./matching.page.scss'],
+  selector: "app-matching",
+  templateUrl: "./matching.page.html",
+  styleUrls: ["./matching.page.scss"],
 })
 export class MatchingPage implements OnInit {
+  currectTeam = {
+    teamName: "Rama ETA",
+    rating: 9999,
+  };
 
-  constructor() { }
+  lsitTeam = [
+    {
+      teamName: "Team A",
+      rating: 5654,
+      result: "Win",
+      score: "3-0",
+      imgTeam: "",
+    },
+    {
+      teamName: "Team B",
+      rating: 12562,
+      result: "Win",
+      score: "2-1",
+      imgTeam: "",
+    },
+    {
+      teamName: "Team H",
+      rating: 7845,
+      result: "Lost",
+      score: "0-3",
+      imgTeam: "",
+    },
+  ];
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  openDetailsWithState(isCaptan: boolean) {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        isCaptan: isCaptan,
+      },
+    };
+    this.router.navigate(["match-result"], navigationExtras);
+    console.log(`isCaptan : ${isCaptan}`);
+    console.log(isCaptan);
   }
-
 }
